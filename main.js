3a71f66cff627bfbf04e0a08f5844dcd9f43a6d7
Marvin Loreto <marvinjloreto@gmail.com>
2022-12-30 22:19:46 +0000

    Fixed modal, added event listener to both buttons -Marvin

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var addButton = document.getElementById('add-entry');
 var modalHider = document.getElementById('modal-hider');
 var modalBackground = document.querySelector('.back-ground-div');
+var submitButton = document.getElementById('submit-entry');
+
 addButton.addEventListener('click', handleModal);
 function handleModal(event) {
+  toggleView(true);
+}
+
+submitButton.addEventListener('click', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
 
 }
 
