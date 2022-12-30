3338d99e320b6bec5d14af92bf12318dc5c3c733
Sean Hughes <coronapilot69@gmail.com>
2022-12-30 22:53:44 +0000

    added localstorage set and get, retrieved form values in handlesubmit function and pushed the new object to the data.entries array - sean

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ var addButton = document.getElementById('add-entry');
 var modalHider = document.getElementById('modal-hider');
 var modalBackground = document.querySelector('.back-ground-div');
 var submitButton = document.getElementById('submit-entry');
+var day = document.getElementById('days');
+var time = document.getElementById('time');
+var notes = document.getElementById('text-area');
+var data = {
+  entries: []
+};
+
+var previousData = localStorage.getItem('planner-data');
+if (previousData !== null) {
+  data = JSON.parce(previousData);
+}
+window.addEventListener('beforeunload', handleBeforeUnload);
+function handleBeforeUnload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('planner-data', dataJSON);
+}
 
 addButton.addEventListener('click', handleModal);
 function handleModal(event) {
@@ -11,7 +27,11 @@ function handleModal(event) {
 submitButton.addEventListener('click', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
-
+  var formData = {};
+  formData.day = day.value;
+  formData.time = time.value;
+  formData.notes = notes.value;
+  data.entries.push(formData);
 }
 
 function toggleView(show) {
